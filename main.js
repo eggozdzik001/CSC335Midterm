062cfe021b71fb4ca5291453207f4c71272c85dc
Edith Gozdzik <eggozdzik001@cs0.converse.edu>
2019-10-10 16:04:31 -0400

    Midterm stuff

--- main.js
+++ main.js
@@ -4,5 +4,11 @@ window.onload = () => {
     document.getElementById('enable-cvox').click();
 }
 
-function showWeather() {
+function showWeather(); {
+    let targetURL = 'https://cs1.converse.edu/~phbrown/CSC335/weather.php';
+    let fillFn = function (request) {
+        let elt = document.getElementById('showweatherdiv');
+        elt.innerHTML = ""; 
+    };
+    handleRequestOnCompletion(fillFn, targetURL);
 }
